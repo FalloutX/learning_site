d74d6b3ee2721b7e52448b245069a5cabb1b321b
FalloutX <inderjitsidhu@outlook.com>
2016-07-28 01:24:23 +0530

    Add StepInline to the admin panel. This way Course Panel will show constituent Step Models in Admin Panel.

--- courses/admin.py
+++ courses/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Course, Step
 
+
+# Adding the Inlines for Course -> Steps
+
+class StepInline(admin.StackedInline):
+    model = Step
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [StepInline, ]
+
+
 # Register your models here.
-admin.site.register(Course)
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Step)
