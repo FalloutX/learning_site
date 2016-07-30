00a58a74f076204992eb2884bac3c6d09f2cf405
FalloutX <inderjitsidhu@outlook.com>
2016-07-30 22:56:12 +0530

    Add view tests for Course List, Course Detail, & Step Detail views. Add template tests for course_list.html, course_detail.html & step_detail.html template

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.utils import timezone
 from .models import Course, Step
@@ -22,3 +23,45 @@ class CourseModelTests(TestCase):
             course=self.course
         )
         self.assertIn(step, self.course.step_set.all())
+
+class CourseViewsTests(TestCase):
+    def setUp(self):
+        self.course = Course.objects.create(
+            title="Python Regular Expressions",
+            description="Learn to write regular expressions in Python."
+        )
+
+        self.course2 = Course.objects.create(
+            title="Python Testing",
+            description="Learn to write tests in Python"
+        )
+
+        self.step = Step.objects.create(
+            title="Introduction to Docsets",
+            description="Learn to write Tests in your docstrings",
+            course=self.course
+        )
+
+    def test_course_list_view(self):
+        resp = self.client.get(reverse('courses:list'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn(self.course, resp.context['courses'])
+        self.assertIn(self.course2, resp.context['courses'])
+        self.assertTemplateUsed(resp, 'courses/course_list.html')
+        self.assertContains(resp, self.course.title)
+
+    def test_course_detail_view(self):
+        resp = self.client.get(reverse('courses:details', kwargs={'pk': self.course.pk}))
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(self.course, resp.context['course'])
+        self.assertTemplateUsed(resp, 'courses/course_detail.html')
+        self.assertContains(resp, self.course.title)
+
+    def test_step_detail_view(self):
+        resp = self.client.get(reverse('courses:step', kwargs={
+                                    'course_pk': self.course.pk,
+                                    'step_pk': self.step.pk}))
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(self.step, resp.context['step'])
+        self.assertTemplateUsed(resp, 'courses/step_detail.html')
+        self.assertContains(resp, self.step.title)
